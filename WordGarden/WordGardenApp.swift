ca010eaa14f58500170172a7030e75a42df52a84
fvj5054 <fvj5054@gmail.com>
2025-04-07 10:48:48 -0700

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Student1 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
